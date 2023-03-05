1f3522ea878fe01adb0db71197ff67c3d3509833
raudbtw <105887271+raudbtw@users.noreply.github.com>
2023-03-05 12:44:28 +0200

    Update EditPostPage.jsx

--- client-mern-blog/src/pages/EditPostPage.jsx
+++ client-mern-blog/src/pages/EditPostPage.jsx
@@ -65,7 +65,7 @@ export const EditPostPage = () => {
             <div className='flex object-cover py-2'>
                 {oldImage && (
                     <img
-                        src={`http://localhost:3002/${oldImage}`}
+                        src={`https://mern-blog-p366b59pm-raudbtw.vercel.app//${oldImage}`}
                         alt={oldImage.name}
                     />
                 )}
