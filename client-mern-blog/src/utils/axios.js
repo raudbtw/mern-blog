de0566910d3a3b80c23106d31b18b6c9b83cdaae
raudbtw <105887271+raudbtw@users.noreply.github.com>
2023-03-07 16:49:02 +0200

    Update axios.js

--- client-mern-blog/src/utils/axios.js
+++ client-mern-blog/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'mern-blog-api.vercel.app',
+    baseURL: 'https://mern-blog-client-ten.vercel.app/',
 })
 
 instance.interceptors.request.use((config) => {
