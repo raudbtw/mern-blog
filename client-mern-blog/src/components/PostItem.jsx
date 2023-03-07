5a19fe0c31b4923e7c3f9337d6a4a3ccf365d1d5
raudbtw <105887271+raudbtw@users.noreply.github.com>
2023-03-07 20:39:08 +0200

    Update PostItem.jsx

--- client-mern-blog/src/components/PostItem.jsx
+++ client-mern-blog/src/components/PostItem.jsx
@@ -21,7 +21,7 @@ export const PostItem = ({ post }) => {
                 >
                     {post.imgUrl && (
                         <img
-                            src={`mern-blog-api.vercel.app/${post.imgUrl}`}
+                            src={`process.env.REACT_APP_API_URL${post.imgUrl}`}
                             alt='img'
                             className='object-cover w-full'
                         />
